8d7202fc415e25f7a80d39ed2e50ecb964d6d35e
ferfragoso <31105466+ferfragoso@users.noreply.github.com>
2017-08-23 20:58:44 -0500

    Create Practica2.c

---
+++ Practica2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  printf("Hola Mundo");
+  printf("Al fin mi segundo codigo");
+  printf("Programar es facil");
+  printf("Voy a compartir este codigo");
+  return 0;
+  }
